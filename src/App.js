1c4090299bb85625a5e205e5b5f05cd2f2f419b6
DavidWells <hello@davidwells.io>
2018-07-05 14:00:06 -0700

    redirect

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Gated Site 1</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          You need a valid JWT to see this site
         </p>
       </div>
     );
