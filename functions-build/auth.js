b44a27e3d55454190cb5266267fe0d33b9b9f102
DavidWells <hello@davidwells.io>
2018-07-08 20:20:35 -0700

    update auth

--- functions-build/auth.js
+++ functions-build/auth.js
@@ -77,9 +77,14 @@ var _cookie2 = _interopRequireDefault(_cookie);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.handler = (event, context, callback) => {
-  console.log('event', event);
-  console.log('context', context);
-  const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzEwOTAzNjIsInN1YiI6ImZiYWQzNjMyLWY3MGEtNDFiYi04OWVjLTM2Y2NjZGZmMTk2MyIsImVtYWlsIjoiZGF2aWRncmVnb3J5d2VsbHNAZ21haWwuY29tIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwifSwidXNlcl9tZXRhZGF0YSI6eyJmdWxsX25hbWUiOiJEYXZpZCJ9fQ.Hq95G9nbCaTxUfbWZfR0lXkC6vRsu-9lnRkX0bHX3M8';
+  console.log('queryStringParameters', event.queryStringParameters);
+
+  const params = event.queryStringParameters;
+  const authToken = params.token || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzEwOTAzNjIsInN1YiI6ImZiYWQzNjMyLWY3MGEtNDFiYi04OWVjLTM2Y2NjZGZmMTk2MyIsImVtYWlsIjoiZGF2aWRncmVnb3J5d2VsbHNAZ21haWwuY29tIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwifSwidXNlcl9tZXRhZGF0YSI6eyJmdWxsX25hbWUiOiJEYXZpZCJ9fQ.Hq95G9nbCaTxUfbWZfR0lXkC6vRsu-9lnRkX0bHX3M8';
+  const referrer = event.headers.referer;
+  console.log('process.env.URL', process.env.URL);
+  console.log('referer', referrer);
+  console.log('set token', authToken);
 
   const myCookie = _cookie2.default.serialize('nf_jwt', authToken, {
     secure: true,
@@ -91,15 +96,15 @@ exports.handler = (event, context, callback) => {
 
   const cookieResponse = {
     "statusCode": 302,
-    "Location": 'body.url',
+    "Location": process.env.URL,
     "headers": {
       "Set-Cookie": myCookie
-    },
-    "body": "..."
+    }
   };
   console.log('cookieResponse', cookieResponse);
 
-  // return callback(null, cookieResponse);
+  // set cookie and redirect
+  return callback(null, cookieResponse);
 
   return callback(null, {
     statusCode: 200,
--- functions/auth.js
+++ functions/auth.js
@@ -1,9 +1,14 @@
 import cookie from 'cookie'
 
 exports.handler = (event, context, callback) => {
-  console.log('event', event)
-  console.log('context', context)
-  const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzEwOTAzNjIsInN1YiI6ImZiYWQzNjMyLWY3MGEtNDFiYi04OWVjLTM2Y2NjZGZmMTk2MyIsImVtYWlsIjoiZGF2aWRncmVnb3J5d2VsbHNAZ21haWwuY29tIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwifSwidXNlcl9tZXRhZGF0YSI6eyJmdWxsX25hbWUiOiJEYXZpZCJ9fQ.Hq95G9nbCaTxUfbWZfR0lXkC6vRsu-9lnRkX0bHX3M8'
+  console.log('queryStringParameters', event.queryStringParameters)
+
+  const params = event.queryStringParameters
+  const authToken = params.token || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzEwOTAzNjIsInN1YiI6ImZiYWQzNjMyLWY3MGEtNDFiYi04OWVjLTM2Y2NjZGZmMTk2MyIsImVtYWlsIjoiZGF2aWRncmVnb3J5d2VsbHNAZ21haWwuY29tIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwifSwidXNlcl9tZXRhZGF0YSI6eyJmdWxsX25hbWUiOiJEYXZpZCJ9fQ.Hq95G9nbCaTxUfbWZfR0lXkC6vRsu-9lnRkX0bHX3M8'
+  const referrer = event.headers.referer
+  console.log('process.env.URL', process.env.URL)
+  console.log('referer', referrer)
+  console.log('set token', authToken)
 
   const myCookie = cookie.serialize('nf_jwt', authToken, {
     secure: true,
@@ -15,15 +20,15 @@ exports.handler = (event, context, callback) => {
 
   const cookieResponse = {
     "statusCode": 302,
-    "Location" : 'body.url',
+    "Location" : process.env.URL,
     "headers": {
       "Set-Cookie": myCookie
-    },
-    "body": "..."
+    }
   }
   console.log('cookieResponse', cookieResponse)
 
-  // return callback(null, cookieResponse);
+  // set cookie and redirect
+  return callback(null, cookieResponse);
 
   return callback(null, {
     statusCode: 200,
